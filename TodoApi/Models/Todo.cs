aacbf75fa376f6547f13e8ba87b5019d0e473786
Mahalakshmi <mahaguru95@gmail.com>
2026-06-21 16:08:22 +0530

    validation added in model to return bad request

--- TodoApi/Models/Todo.cs
+++ TodoApi/Models/Todo.cs
@@ -1,9 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace TodoApi.Models
 {
     public class Todo
     {
         public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
         public string Title { get; set; }
+        [MaxLength(500)]
         public string Description { get; set; }
         public bool IsCompleted { get; set; }
         public DateTime CreatedAt { get; set; }
---
+++ TodoApi/Models/TodoRequest.cs
@@ -0,0 +1,10 @@
+namespace TodoApi.Models
+{
+    public class UpdateTodoRequest
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
